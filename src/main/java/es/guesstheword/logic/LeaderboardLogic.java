247c292ec54959acaf646e76da115a450f956fe1
EmSley77 <emanuelsleyman@gmail.com>
2024-07-04 23:27:18 +0200

    done with single user stats

--- src/main/java/es/guesstheword/logic/LeaderboardLogic.java
+++ src/main/java/es/guesstheword/logic/LeaderboardLogic.java
@@ -90,13 +90,35 @@ public class LeaderboardLogic {
 
         }
 
+        int wins = 0;
+        int losses = 0;
+        int gamesPlayed = 0;
+        int avgGuess = 0;
         for (Leaderboard leaderboard : leaderboards) {
 
             if (userLogic.getUserId() == leaderboard.getUserId()) {
 
 
+                if (leaderboard.getWinLoss().equals("WON")) {
+                    wins += 1;
+                }
+                if (leaderboard.getWinLoss().equals("LOST")) {
+                    losses += 1;
+                }
+
+
+                gamesPlayed++;
+
             }
+            avgGuess += leaderboard.getNumberOfGuesses() / gamesPlayed;
         }
+        System.out.println("______________________________________");
+        System.out.println("USERNAME: " + user.getUsername());
+        System.out.println("LEVEL: " + user.getGameLevel());
+        System.out.println("WINS: " + wins);
+        System.out.println("GAMES LOST: " + losses);
+        System.out.println("AVERAGE GUESS PER GAME: " + avgGuess);
+        System.out.println("TOTAL GAMES PLAYED: " + gamesPlayed);
 
 
     }
